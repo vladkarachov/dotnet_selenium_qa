04115bd9b6893caaa8aea2b76e532b63759bcc38
Vlad <Vlad@VLADLAPTOP>
2020-04-29 14:51:44 +0300

    added test for non-excisting element

---
+++ UnitTestProject1/no_exception_el_checker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+using NUnit.Framework;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    class no_exception_el_checker
+    {
+        [Test]
+        public void find_element()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://greenforest.com.ua/courses/");
+            var excitance = driver.FindElements(By.Id("not_excisting_element")).Count() != 0;
+            Assert.IsFalse(excitance);
+        }
+    }
+}
+
